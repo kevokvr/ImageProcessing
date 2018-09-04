7a4d8f29f6b26a73a9972f526bda2fbba24e8c45
kevokvr <kvalenzu@msudenver.edu>
2018-09-04 14:53:23 -0600

    Circle Border

---
+++ CircleAround/circle_around.m
@@ -0,0 +1,27 @@
+clc
+clear all
+close all
+
+im=imread('bobby.png');
+imshow(im);
+
+grayim=rgb2gray(im);
+imshow(grayim);
+
+[r,c]=size(grayim);
+radius = 150;
+
+%center = [250,191];
+
+for r=1:r
+    for c=1:c
+        if sqrt((r-192)^2 + (c-250)^2)<radius
+            grayim(r,c)=grayim(r,c);
+        else
+            grayim(r,c)=0;
+        end
+    end
+end
+
+figure, imshow(grayim);
+
