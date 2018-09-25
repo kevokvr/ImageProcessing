50f09f8c6fdffc0e3e09096d9a8ef9e4b21a3dbe
kevokvr <kvalenzu@msudenver.edu>
2018-09-25 17:09:31 -0600

    Fixed Edge detection

--- FilterIris/Filters.m
+++ FilterIris/Filters.m
@@ -5,22 +5,64 @@ close all
 im = imread('dylan.jpeg');
 im = rgb2gray(im);
 noisyim = imnoise(im,'salt & pepper',0.02);
-imshow(noisyim);
+% imshow(noisyim);
 
 %Applying average filter
 w=[1 1 1; 1 1 1; 1 1 1]/9; 
 im2=imfilter(noisyim,w,'same'); 
-figure,imshow(im2);
+% figure,imshow(im2);
 
+% Applying own filter
+Vertical = [-1 0 1 ; -2 0 2 ; -1 0 1];
+ imx = imfilter(im, Vertical,'same','replicate'); 
+%  figure, imshow(imx);
+
+ 
+ %Applying own filter
+ Horizantal = [-1 -2 -1; 0 0 0 ; 1 2 1];
+im6 = imfilter(im, Horizantal,'same','replicate');
+figure, imshow(im6);
+         
 %Applying median filter
 im3 = medfilt2(noisyim);
-figure, imshow(im3);
+% figure, imshow(im3);
 
 %Applying sobel filter
 im4 = edge(im,'sobel');
-figure, imshow(im4)
+% figure, imshow(im4)
 
 %Applying laplacian
 H = fspecial('laplacian',0.5);
 lap = imfilter(im,H,'same');
-figure, imshow(lap)
+% figure, imshow(lap)
+
+
+% figure,subplot(2,3,1),imagesc(im2);
+% colormap(gray(256));
+% axis image; 
+% axis off;
+% 
+% subplot(2,3,2),imagesc(im3);
+% colormap(gray(256));
+% axis image;
+% axis off;
+% 
+% subplot(2,3,3),imagesc(im4);
+% colormap(gray(256));
+% axis image;
+% axis off;
+% 
+% subplot(2,3,4),imagesc(lap);
+% colormap(gray(256));
+% axis image;
+% axis off;
+% 
+% subplot(2,3,5),imagesc(imx);
+% colormap(gray(256));
+% axis image;
+% axis off;
+% 
+% subplot(2,3,6),imagesc(im6);
+% colormap(gray(256));
+% axis image;
+% axis off;
--- FilterIris/IrisBoundary.m
+++ FilterIris/IrisBoundary.m
@@ -1,30 +1,113 @@
 clc
 clear all
 close all
+%Kevin Valenzuela
+Original = imread('iris.bmp');
+Org2Double = double(Original);
+Eye = Org2Double(1:2:end,1:2:end);
 
-I = imread('iris.bmp');
-imshow(I,[]);
-[X,Y]=size(I);
+Horizantal = [-1 -2 -1
+                0 0 0 
+                 1 2 1];
+  
+Vertical = [-1 0 1 
+             -2 0 2 
+             -1 0 1];
 
-[E, thresh] = edge(I, 'sobel');
-figure, imshow(E, []);
+temp1 = imfilter(Eye, Horizantal,'same','replicate');
+temp2 = imfilter(Eye, Vertical,'same','replicate'); 
+EyeEdges = sqrt((temp1.^2+temp2.^2)/2);
 
-H=zeros(X,Y);
+r=20;
+innercircle=zeros(r*2+1);
+innercircle(1,r+1)=1;
+innercircle(r*2+1,r+1) = 1;
+oneoverR=1/r;
 
-for x=1:X
-    for y=1:Y
-        if E(x,y)
-            for x0=1:X
-                for y0=1:Y
-                    r = sqrt((x-x0)^2 + (y-y0)^2)
-                    H(x0,y0) = H(x0,y0)+ 1;
-                end
-            end
+for i=0:oneoverR:pi/2
+    y=round(r*cos(i));
+    x=round(r*sin(i));
+    innercircle(r+1+x,r+1+y)=1;
+    innercircle(r+1+x,r+1-y)=1;
+    innercircle(r+1-x,r+1+y)=1;
+    innercircle(r+1-x,r+1-y)=1;
+end
+
+r=20; 
+circlesize=r*2+1+6;
+center=round(0.5*circlesize);
+anothercircle=zeros(r+4);
+
+for i=1:circlesize
+    for j=1:circlesize
+        if(sqrt((i-center)^2+(j-center)^2) < (r+3)) && (sqrt((i-center)^2+(j-center)^2) > (r+3))
+            anothercircle(i,j)=1; 
         end
     end
 end
 
-plot(H);
+kernel=anothercircle;
+eye3=imfilter(EyeEdges,kernel,'same');
+[fr,fc]=find(eye3>0.9*max(eye3(:)));
+cenrow=round(mean(fr));
+cencol=round(mean(fc));
+
+% clearEdgeEye = zeros(size(EyeEdges,1),size(EyeEdges,2));
+clearEdgeEye = (EyeEdges >0.6*max(EyeEdges(:)));
+
+
+for i=j:size(EyeEdges,1)
+    for j=1:size(EyeEdges,2)
+        if sqrt((i-cenrow)^2+(j-cencol)^2)>25
+            clearEdgeEye(i,j)=0;
+        end
+    end
+end
+
+iris = clearEdgeEye;
+
+[r,c]=size(iris);
+radius = 30;
+
+for r=1:r
+    for c=1:c
+        if sqrt((r-68)^2 + (c-92)^2)>radius
+            iris(r,c)=0;
+        end
+    end
+end
+
+
+
+figure,subplot(2,2,1),imagesc(Eye);
+colormap(gray(256));
+axis image; 
+axis off;
+
+subplot(2,2,2),imagesc(EyeEdges);
+colormap(gray(256));
+axis image;
+axis off;
+
+subplot(2,2,3),imagesc(clearEdgeEye);
+colormap(gray(256));
+axis image;
+axis off;
+
+subplot(2,2,4),imagesc(iris);
+colormap(gray(256));
+axis image;
+axis off;
+
+
+
+
+    
+    
+
+
+
+
 
 
  
